ddd98057db3af3a09dc63221c3e3dada39cf848f
Carlos Rojas Vega <karlosrvxx@gmail.com>
2023-08-01 15:36:47 -0500

    update4

--- components/Search.js
+++ components/Search.js
@@ -1,6 +1,6 @@
 'use client'
 import Image from 'next/image'
-// import styles from './sideBar.css'
+import styles from './sideBar.css'
 import { useState } from 'react';
 import React from 'react'
 
